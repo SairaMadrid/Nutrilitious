ed3bc7308a4a6901b3eb4448d772d2df51a6baee
SairaMadrid <sairakbar@gmail.com>
2023-07-11 19:14:48 +0200

    Navbar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,29 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Register from './components/Register.jsx';
-import Login from './components/Login.jsx';
-import Results from './components/Results';
-import RecipeCard from './components/RecipeCard';
-import Favourites from './components/Favourites';
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+import Results from "./components/Results.jsx";
+import RecipeCard from "./components/RecipeCard.jsx";
+import Favourites from "./components/Favourites.jsx";
+import Profile from "./components/Profile.jsx";
 
 function App() {
-  
-
   return (
     <div className="wrapper">
       <h1>NutriApp</h1>
-      
+      <h3>Get cooking!</h3>
+
       <Routes>
-        <Route path="/register" element={<Register />}>  
-      
-      </Route>
-      <Route path="/home" element={<Login />}>  
-      
-      </Route>
-      <Route path="/results" element={<Results />}>  
-      
-      </Route>
-      <Route path="/recipes/:id" element={<RecipeCard />}>  
-      
-      </Route>
-      <Route path="/favourites" element={<Favourites />}>  
-      
-      </Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/home" element={<Login />}></Route>
+        <Route path="/results" element={<Results />}></Route>
+        <Route path="/recipes/:id" element={<RecipeCard />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/favourites" element={<Favourites />}></Route>
       </Routes>
-      
     </div>
-    
   );
-};
+}
 
-export default App
-
-
-
-
-
-
+export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-
+import useAuth from "../hooks/useAuth";
+​
 export default function Navbar() {
-    return //links need to be added here
-    
-  }
+  const auth = useAuth();
+​
+  const logout = () => {
+    auth.logout();
+  };
+​
+  return (
+    <>
+      {!auth.isLoggedIn && (
+        <div>
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+          <div>
+            <Link to="/register">Register</Link>
+          </div>
+          {/* do we want the searchbar to be seen here? */}
+          <div>
+            <Link to="/searchbar">Search</Link>
+          </div>
+          
+        </div>
+      )}
+​
+      {auth.isLoggedIn && (
+        <div>
+          <div>
+            <Link to="/dashboard">Dashboard</Link>
+          </div>
+          <div>
+            <Link to="/favourites">Favourites</Link>
+          </div>
+          <div>
+            <Link to="/results">Search results</Link>
+          </div>
+          <div>
+            <Link to="/profile">Profile</Link>
+          </div>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
