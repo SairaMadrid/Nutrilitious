a40ae94556c58d5072994fed317eec40b271462b
SairaMadrid <sairakbar@gmail.com>
2023-07-13 22:57:47 +0200

    Favourites removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Register from "./components/Register.jsx";
-import Login from "./components/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 import Results from "./components/Results.jsx";
 import RecipeCard from "./components/RecipeCard.jsx";
-import Favourites from "./components/Favourites.jsx";
+
 import Navbar from "./components/Navbar.jsx";
 import Homepage from "./pages/Homepage";
-import Profile from "./components/Profile";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/results" element={<Results />}></Route>
         <Route path="/recipes/:id" element={<RecipeCard />}></Route>
-        <Route path="/favourites" element={<Favourites />}></Route>
+
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </div>
