eedc54f679227fb6eef8024064cc69c1e970bd0f
SairaMadrid <sairakbar@gmail.com>
2023-07-13 22:32:45 +0200

    Profile.jsx code deleted

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,8 +2,4 @@ import React from "react";
 
 export default function Profile() {
   return <h2>Profile</h2>;
-
-  import { useState, useEffect, useContext, createContext } from "react";
-
-  return <div>Profile</div>;
 }
