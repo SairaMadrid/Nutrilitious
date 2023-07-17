f8fa55dcbd858e3b7dbc75ed3171881f31f29964
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-07-17 21:36:40 +0200

    Minor tweaks in the init file and SearchBar placeholder text

--- model/init_db.sql
+++ model/init_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE `profiles`(
     `last_name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    `preference` TEXT NOT NULL,
+    `preferences` TEXT NOT NULL,
     `cooking_skills` TEXT NOT NULL,
     `description` TEXT NOT NULL,
     UNIQUE KEY `profiles_email_unique`(`email`),
@@ -22,5 +22,3 @@ CREATE TABLE `favourites`(
     PRIMARY KEY(`id`),
     CONSTRAINT `favourites_profiles_id_foreign` FOREIGN KEY (`profiles_id`) REFERENCES `profiles`(`id`)
 );
-
-INSERT INTO profiles (first_name, last_name, email, password, preference, cooking_skills, description) VALUES ('Stephanie', 'Anko-Hubik', 'sah@sah.com', 'never_use_123', 'vegetarian', 'Novice chef', 'Likes to eat healthily'), ('Saira', 'Akbar', 'sa@sa.com', 'never_use_000', 'kids-friendly', 'Hobby chef', 'The kitchen is the heart of a home'), ('Irene', 'Kulikova', 'ik@ik.com', 'never_use_777', 'soul food', 'Competent chef', 'Likes to experiment with different cuisines'), ('Carli', 'McCarthy', 'cmcc@cmcc.com', 'never_use_111', 'lactose-free', 'Expert chef', 'On the lookout for dairy-free alternative recipes');
