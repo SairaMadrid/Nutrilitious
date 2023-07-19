cc90109ba0a931889cc390b15627ffe819e5b92e
SairaMadrid <sairakbar@gmail.com>
2023-07-19 11:29:28 +0200

    debugging

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,48 +28,58 @@ export default function Login() {
   };
 
   return (
-
     <div className="container text-center">
       <h1 className="pb-4">Welcome Back!</h1>
-      <div class="row">
-      <form action="" onSubmit={handleLogin}>
-      <div className="col-sm-4 offset-sm-4 mt-2 mb-4">
-      <label className="form-label start">Email</label>
-      <div class="form-floating">
-        <input
-          className="form-control"
-          value={email}
-          onChange={handleEmailChange}
-          name="username"
-          type="text"
-          placeholder="email"
-        />
-        <label className="form-label text-sm">Enter Email</label>
-        </div>
-        </div>
-        <div className="col-sm-4 offset-sm-4 my-3">
-        <label className="form-label start">Password</label>
-        <div class="form-floating">
-        <input
-          className="form-control"
-          value={password}
-          onChange={handlePasswordChange}
-          name="password"
-          type="password"
-          placeholder="password"
-        />
-           <label className="form-label text-sm">Enter Password</label>
-        </div>
-        </div>
-        <p><a className="link" href="#">Forgot password?</a></p>
+      <div className="row">
+        <form action="" onSubmit={handleChange}>
+          <div className="col-sm-4 offset-sm-4 mt-2 mb-4">
+            <label className="form-label start">Email</label>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                value={email}
+                onChange={handleChange}
+                name="username"
+                type="text"
+                placeholder="email"
+              />
+              <label className="form-label text-sm">Enter Email</label>
+            </div>
+          </div>
+          <div className="col-sm-4 offset-sm-4 my-3">
+            <label className="form-label start">Password</label>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                value={password}
+                onChange={handleChange}
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+              <label className="form-label text-sm">Enter Password</label>
+            </div>
+          </div>
+          <p>
+            <a className="link" href="#">
+              Forgot password?
+            </a>
+          </p>
 
-        <button className="btn btn-success my-2 py-2 col-12 col-sm-4" onClick={handleLogin}>
-        Sign in<i className="fa-solid fa-arrow-right ms-3"></i>
-        </button>
-      </form>
+          <button
+            className="btn btn-success my-2 py-2 col-12 col-sm-4"
+            onClick={handleChange}
+          >
+            Sign in<i className="fa-solid fa-arrow-right ms-3"></i>
+          </button>
+        </form>
       </div>
-      <p className="py-3">Don't have an account? <a className="link" href="/register">Sign up</a></p>
-
+      <p className="py-3">
+        Don't have an account?{" "}
+        <a className="link" href="/register">
+          Sign up
+        </a>
+      </p>
     </div>
   );
 }
