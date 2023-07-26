7728f06ed81f27d284cd701193331ba8c71da3e5
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-07-26 21:07:15 +0200

    final styling recipe card

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -12,27 +12,43 @@ export default function SearchBar({ onSearch }) {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col col-md-4 offset-sm-4 ">
-          <div className="form-floating">
-            <input
-              className="form-control my-2"
-              type="text"
-              placeholder="Search by ingredients..."
-              value={searchItems}
-              onChange={handleChange}
-            />{" "}
-            <label className="form-label text-sm">Search by ingredients</label>
+    <>
+      <h2 className="homepage-header py-4 fw-semibold">
+        Explore Endless Culinary Possibilities
+        <i className="fa-solid fa-utensils mx-2"></i>
+      </h2>
+      <p>
+        Nutrilitious offers a wide array of recipes that cater to various
+        tastes, dietary preferences, and cuisines.
+      </p>
+      <p>
+        From savory to sweet, vegetarian to keto-friendly, we have something for
+        everyone.
+      </p>
+      <div className="container">
+        <div className="row">
+          <div className="col col-md-4 offset-sm-4  ">
+            <div className="form-floating">
+              <input
+                className="form-control my-2"
+                type="text"
+                placeholder="Search by ingredients..."
+                value={searchItems}
+                onChange={handleChange}
+              />{" "}
+              <label className="form-label text-sm">
+                Search by ingredients
+              </label>
+            </div>
           </div>
         </div>
+        <button
+          className="search-button btn btn-success my-2"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
       </div>
-      <button
-        className="search-button btn btn-success my-2"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
-    </div>
+    </>
   );
 }
